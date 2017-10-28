81892d3e0fbfae48cee21c6f2ed9240490590858
Gabriel Teston <gabriel_teston@hotmail.com>
2017-10-27 22:51:30 -0200

    Simple XOR learning Neural Network

---
+++ Example.py
@@ -0,0 +1,14 @@
+from NeuralNetwork import *
+
+# Just a XOR example and how should be the training dataset. Sklearn datasets follow bassicaly this pattern
+class Dataset:
+    def __init__(self):
+        self.data = np.array([[0, 0],
+                              [0, 1],
+                              [1, 0],
+                              [1, 1]])
+        self.target = np.array([[0], [1], [1], [0]])
+
+dataset = Dataset()
+nl = NeuralNetwork(dataset=dataset, layers_format=[2, 10, 10, 1], learning_rate=0.5, momentum=1, tol=0.01)
+nl.fit()
